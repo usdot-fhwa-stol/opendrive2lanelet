c0d8a94ae0067d9e63b5862682326b3f18b45d88
Alex Frye <65964406+afrye-leidos@users.noreply.github.com>
2020-07-30 09:42:52 -0600

    Solution to improve the convertor performance issue (#4)
    * Implemented a solution to improve the convertor performance using a hash table
    
    * Fixed comments and imports
    
    * Added more comments

--- opendrive2lanelet2/convertor/opendrive2lanelet2convertor.py
+++ opendrive2lanelet2/convertor/opendrive2lanelet2convertor.py
@@ -37,6 +37,7 @@ class Opendrive2Lanelet2Convertor:
         self.ways = xml.Element('ways')
         self.relations = xml.Element('relations')
         self.all_nodes = []
+        self.all_nodes_dict = dict()
         self.all_ways = []
 
     def open_drive_loader(self, fn):
@@ -77,7 +78,7 @@ class Opendrive2Lanelet2Convertor:
 
         dom = pxml.parse(fn)
         pretty_xml_as_string = dom.toprettyxml()
-        fn =  fn.replace(".osm","")
+        fn = fn.replace(".osm","")
 
         fh = open(fn + "_pretty.osm", "w+")
         fh.write(pretty_xml_as_string)
@@ -91,17 +92,54 @@ class Opendrive2Lanelet2Convertor:
         local_x, local_y = x, y
         return Node(node_id,lat,lon, local_x, local_y)
 
-    def calc_distance_node_tresh(self,n1,n2):
-        if(abs(n1.lat - n2.lat) < 0.000000001 and abs(n1.lon - n2.lon) < 0.000000001):
-            return True
-        else:
-            return False
-    
-    def calc_distance_node_isclose(self,n1,n2):
-        if(math.isclose(n1.lat, n2.lat, rel_tol=1e-10) and math.isclose(n1.lon, n2.lon, rel_tol=1e-10)):
-            return True
-        else:
-            return False
+    # The key for the dictionary will be of the form: 'lat_lon',
+    # where lat and lon are integers (lat here = input latitude / threshold) ,
+    # and each have 12 number slots associated.
+    # This number is set for the current threshold of 0.00000001,
+    # where the maximum longitude value of -180 turns into: -18000000000,
+    # filling up all 12 slots. ex: for the point [179.992125274, -45.12893761],
+    # the key is: '017999212527_-04512893761'
+    def key_from_latlon_prec(self, point):
+        return f'{int(np.round(point[0],0)):012d}' + '_' + f'{int(np.round(point[1],0)):012d}'
+
+
+    # Uses a dictionary, self.all_nodes_dict, which contains key-value pairs for all existing points
+    # If there is an existing point within the threshold distance, it returns True and the index of that point
+    # If not, it adds the point to the dictionary and returns False
+    # Inputs:
+    #   new       - Array of lat, lon location in units of degrees. ex: [179.992125274, -45.12893761]
+    #   tolerance - Float distance threshold for calling two points the same. In units of degrees in lat and lon
+    # Returns:
+    #   is_dup    - Boolean, true if the value is a duplicate and false if it isn't
+    #   index     - Integer index of the duplicate value in the self.all_nodes array.
+    #                   Set to None if the value is not a duplicate
+    def check_duplicate_hash_precise(self, new, tolerance=0.00000001):
+        # Grab 5 relative points {-1.1, -0.5, 0, 0.5, 1} in both latitude and longitude
+        new = new / tolerance
+        thresh_range = [-1.1, -0.5, 0, 0.5, 1.1]
+        for lat_threshold in thresh_range:
+            for lon_threshold in thresh_range:
+                threshold = np.array([lat_threshold, lon_threshold])
+                test = new + np.round(threshold, 8)
+
+                # The float -> int conversion will fail if the input value is infinite: float('inf')
+                # If that happens, asssume that it isn't a duplicate and return that is isn't a duplicate
+                try:
+                    test_key = self.key_from_latlon_prec(test)
+                except OverflowError:
+                    print('Lat or lon over max value')
+                    print('value is: ' + str([new * tolerance]))
+                    return False, None
+
+                if test_key in self.all_nodes_dict:
+                    [point, value] = self.all_nodes_dict[test_key]
+                    difference = np.abs(point - new)
+                    if difference[0] < 1 and difference[1] < 1:
+                        return True, value
+        test_key = self.key_from_latlon_prec(new)
+        value = len(self.all_nodes_dict)
+        self.all_nodes_dict[test_key] = [new, value]
+        return False, None
 
     # apply convert_vertice_to_node to a list of nodes
     # bound_id 0,1 for left and right 
@@ -110,43 +148,42 @@ class Opendrive2Lanelet2Convertor:
         for j in range(len(vertices)):
             node_id = relation_id + str(bound_id) + "{0:0=3d}".format(j)
             node = self.convert_vertice_to_node(node_id, vertices[j])
+            node_latlon = np.array([node.lat, node.lon])
 
-            duplicate_flag = False
-            for i in range(len(self.all_nodes)):
-                if(self.calc_distance_node_tresh(node,self.all_nodes[i])):
-                    nodes.append(self.all_nodes[i])
-                    duplicate_flag = True
-                    break
-
-                if(self.calc_distance_node_isclose(node,self.all_nodes[i])):
-                    nodes.append(self.all_nodes[i])
-                    duplicate_flag = True
-                    break
-
-            if(duplicate_flag==False):
+            is_dup, index = self.check_duplicate_hash_precise(node_latlon)
+            if is_dup:
+                nodes.append(self.all_nodes[index])
+            else:
                 nodes.append(node)
                 self.nodes.append(node.create_xml_node_object())
                 self.all_nodes.append(node)
+
         return nodes
 
     # check for way duplication in the entire map
+    # For all ways in the map, compare the nodes of that way to the nodes of the current way
+    # The comparison is done by rounding the local position of each node to 3 decimal places
+    # (points match if they are within 1mm)
+    # If more than 5 nodes match, and more than 80% of the nodes match, it is considered a duplicate
+    # and the way already in the map is used instead of the new one
     def check_way_duplication(self,nodes,way):
-	# For a duplicate, need at least 5, and at least 80% matching points
+        # For a duplicate, need at least 5 and at least 80% matching points
+        # A small way may only have a few points, but we don't want to accidentally match large ways
         intersection_test_tresh = 5
         intersection_test_tresh_ratio = 0.8
 
         for k in self.all_ways:
-	    # Get list of points in existing and candidate ways
+            # Get list of points in existing and candidate ways
             test_pts = np.array([[np.round(j.local_x, 3), np.round(j.local_y, 3)] for j in k.nodes])
             pts = np.array([[np.round(j.local_x, 3), np.round(j.local_y, 3)] for j in nodes])
 
-	    # Count the number and ratio of duplicate points
+            # Count the number and ratio of duplicate points
             combined = np.concatenate((test_pts, pts))
             tmp, indices = np.unique(combined, axis=0, return_index=True)
             num_matches = np.shape(combined)[0] - len(indices)
             ratio_matches = 2 * num_matches / np.shape(combined)[0]
 
-	    # If the ratio and count tests pass, re-use the esisting way
+            # If the ratio and count tests pass, re-use the esisting way
             if num_matches > intersection_test_tresh:
                 if ratio_matches > intersection_test_tresh_ratio:
                     return k
@@ -154,7 +191,6 @@ class Opendrive2Lanelet2Convertor:
 
     def convert(self, fn):
         for i in self.scenario._id_set:
-
             left_nodes = []
             right_nodes = []
             relation_id = str(i)
@@ -182,7 +218,7 @@ class Opendrive2Lanelet2Convertor:
 
             from_cad_id = self.scenario._lanelet_network._lanelets[i]._successor
             to_cad_id = self.scenario._lanelet_network._lanelets[i]._predecessor
-            
+
             speed_limit_regulatory_id = relation_id +'00'
             speed_limit_regulatory = SpeedLimitRegulatory(speed_limit_regulatory_id, relation_id, str(max_speed))
 
@@ -190,4 +226,5 @@ class Opendrive2Lanelet2Convertor:
 
             self.relations.append(relation.create_xml_relation_object())
             self.relations.append(speed_limit_regulatory.create_xml_speed_limit_regulatory_object())
+
         self.write_xml_to_file(fn)
